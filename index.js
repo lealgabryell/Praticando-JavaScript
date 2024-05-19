82e33fa72b0e58d75553116c8c7c4084e97eea4e
gabryell leal <g.lealrocha3@gmail.com>
2024-05-19 14:03:05 -0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const prompt = require("prompt-sync")();
-exit
-prompt("digite seu nome")
+// const num1 = prompt("numero 1");
+// const num2 = prompt("numero 2");
+// const num3 = prompt("numero 3");
+// let media;
+// media = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3))/3
+
+// console.log(media); 
+
+
